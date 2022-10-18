88d1e1570a7cf42fef29424d92076c98a7d3a036
abderrahim boudounit <abderrahimboudounit@gmail.com>
2022-10-18 17:15:31 +0100

    ex00

--- solutions/module00/ex00/megaphone.cpp
+++ solutions/module00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 16:19:28 by aboudoun          #+#    #+#             */
-/*   Updated: 2022/10/09 18:20:30 by aboudoun         ###   ########.fr       */
+/*   Updated: 2022/10/18 17:13:19 by aboudoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,15 @@ int	main(int ac, char **av)
 	i = 1;
 	if (ac < 2)
 		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
-	while (av[i])
+	else
 	{
-		j = -1;
-		while (av[i][++j])
-			std::cout<<(char)toupper(av[i][j]);
+		while (av[i])
+		{
+			j = -1;
+			while (av[i][++j])
+				std::cout<<(char)toupper(av[i][j]);
+			i++;
+		}
 		std::cout << std::endl;
-		i++;
 	}
 }
