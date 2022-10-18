b3ee47ab0ca44ff4534146e7605a38b3e470748c
abderrahim <abderrahimboudounit@gmail.com>
2022-10-18 20:02:28 +0100

    starting ex04

---
+++ solutions/module01/ex04/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int	main(int ac, char **av)
+{
+	string s1;
+	string s2;
+
+	if (ac != 4)
+	{
+		cout<<"usage: ./replace <filename> <s1> <s2>"<<endl;
+		return (0);
+	}
+	else
+	{
+
+	}
+
+}
