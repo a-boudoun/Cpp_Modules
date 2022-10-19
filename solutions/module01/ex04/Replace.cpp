c01ed33f9f8c10128c531ba9da0833bf2f3c1974
abderrahim <abderrahimboudounit@gmail.com>
2022-10-19 19:29:48 +0100

    ex04 done

--- solutions/module01/ex04/Replace.cpp
+++ solutions/module01/ex04/Replace.cpp
@@ -10,5 +10,30 @@ Replace::Replace(std::string name)
 
 void	Replace::sedPro(std::string toFind, std::string NewOne)
 {
-	
+	std::string content;
+	std::ifstream myFile(this->infile);
+	if (myFile.is_open())
+	{
+		std::getline(myFile, content, '\0');
+		if (content.empty())
+		{
+			std::cout<<"empty file"<<std::endl;
+			return ;
+		}
+		std::ofstream NewFile(this->outfile);
+		size_t	p;
+		p = content.find(toFind);
+		while ( p != std::string::npos ) {
+			content.erase(p, toFind.length());
+			content.insert(p, NewOne);
+			p = content.find(toFind);
+		}
+		NewFile << content;
+		NewFile.close();
+	}
+	else
+	{
+		std::cerr<<"please make sure the file is there"<<std::endl;
+	}
+	myFile.close();
 }
